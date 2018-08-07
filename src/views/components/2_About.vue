de324061c96b525f47343f7d4d3e44d0c1384804
Vitor Miranda <vcmiranda@outlook.com>
2018-08-06 20:21:39 -0400

    Added download resume feature

--- src/views/components/2_About.vue
+++ src/views/components/2_About.vue
@@ -1,5 +1,12 @@
 <template>
   <v-container fluid class="ma-0 pa-0">
+    <v-layout row wrap fill-height class="mx-auto align-center" style="max-width: 1200px;">
+      <v-flex xs12 class="text-xs-center">
+        <a href="/docs/VitorMiranda_Resume.pdf" style="text-decoration: none;" download>
+          <v-btn>Download My Resume</v-btn>
+        </a>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
--- vue.config.js
+++ vue.config.js
@@ -14,5 +14,20 @@ module.exports = {
         },
       ),
     ],
+    module: {
+      rules: [
+        {
+          test: /\.(pdf)(\?.*)?$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                name: 'docs/[name].[hash:8].[ext]',
+              },
+            },
+          ],
+        },
+      ],
+    },
   },
 };
